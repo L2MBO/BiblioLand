c3494ef3c47c3d2779f694bfdbd1f332cb9e8738
L2MBO <157141954+L2MBO@users.noreply.github.com>
2025-07-01 02:10:55 +0300

    WriteNotifyForm design is ready

--- Biblio/AppForms/AdminForms/WriteNotifyForm.Designer.cs
+++ Biblio/AppForms/AdminForms/WriteNotifyForm.Designer.cs
@@ -29,11 +29,197 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.FormRounded = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.closeButton = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.sendNotifyButton = new Guna.UI2.WinForms.Guna2Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titleTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FormRounded
+            // 
+            this.FormRounded.AnimateWindow = true;
+            this.FormRounded.AnimationInterval = 200;
+            this.FormRounded.BorderRadius = 35;
+            this.FormRounded.ContainerControl = this;
+            this.FormRounded.DockIndicatorTransparencyValue = 0.6D;
+            this.FormRounded.DragEndTransparencyValue = 0.2D;
+            this.FormRounded.DragForm = false;
+            this.FormRounded.HasFormShadow = false;
+            this.FormRounded.TransparentWhileDrag = true;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.guna2Panel1.BorderRadius = 20;
+            this.guna2Panel1.BorderThickness = 1;
+            this.guna2Panel1.Controls.Add(this.descriptionLabel);
+            this.guna2Panel1.Controls.Add(this.descriptionTextBox);
+            this.guna2Panel1.Controls.Add(this.titleTextBox);
+            this.guna2Panel1.Controls.Add(this.nameLabel);
+            this.guna2Panel1.Controls.Add(this.closeButton);
+            this.guna2Panel1.Controls.Add(this.sendNotifyButton);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(245, 285);
+            this.guna2Panel1.TabIndex = 4;
+            // 
+            // closeButton
+            // 
+            this.closeButton.AllowDrop = true;
+            this.closeButton.Animated = true;
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.closeButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.closeButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.closeButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.closeButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(27)))), ((int)(((byte)(30)))));
+            this.closeButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Image = global::Biblio.Properties.Resources.whiteXmark;
+            this.closeButton.ImageSize = new System.Drawing.Size(10, 10);
+            this.closeButton.Location = new System.Drawing.Point(210, 2);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(39)))), ((int)(((byte)(43)))));
+            this.closeButton.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.closeButton.Size = new System.Drawing.Size(33, 33);
+            this.closeButton.TabIndex = 33;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // sendNotifyButton
+            // 
+            this.sendNotifyButton.Animated = true;
+            this.sendNotifyButton.BorderRadius = 17;
+            this.sendNotifyButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sendNotifyButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.sendNotifyButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.sendNotifyButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.sendNotifyButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.sendNotifyButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(132)))), ((int)(((byte)(246)))));
+            this.sendNotifyButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sendNotifyButton.ForeColor = System.Drawing.Color.White;
+            this.sendNotifyButton.Location = new System.Drawing.Point(114, 233);
+            this.sendNotifyButton.Name = "sendNotifyButton";
+            this.sendNotifyButton.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(120)))), ((int)(((byte)(224)))));
+            this.sendNotifyButton.Size = new System.Drawing.Size(97, 36);
+            this.sendNotifyButton.TabIndex = 37;
+            this.sendNotifyButton.Text = "Отправить";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 20);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Новое уведомление";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.titleTextBox.BorderRadius = 17;
+            this.titleTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.titleTextBox.DefaultText = "";
+            this.titleTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.titleTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.titleTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.titleTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.titleTextBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(27)))), ((int)(((byte)(30)))));
+            this.titleTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.titleTextBox.FocusedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(39)))), ((int)(((byte)(43)))));
+            this.titleTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.titleTextBox.ForeColor = System.Drawing.Color.White;
+            this.titleTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.titleTextBox.Location = new System.Drawing.Point(35, 84);
+            this.titleTextBox.MaxLength = 30;
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.PlaceholderText = "";
+            this.titleTextBox.SelectedText = "";
+            this.titleTextBox.Size = new System.Drawing.Size(176, 36);
+            this.titleTextBox.TabIndex = 42;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameLabel.ForeColor = System.Drawing.Color.White;
+            this.nameLabel.Location = new System.Drawing.Point(32, 66);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(77, 15);
+            this.nameLabel.TabIndex = 41;
+            this.nameLabel.Text = "Заголовок";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descriptionLabel.ForeColor = System.Drawing.Color.White;
+            this.descriptionLabel.Location = new System.Drawing.Point(32, 129);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(72, 15);
+            this.descriptionLabel.TabIndex = 52;
+            this.descriptionLabel.Text = "Описание";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Animated = true;
+            this.descriptionTextBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.descriptionTextBox.BorderRadius = 10;
+            this.descriptionTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.descriptionTextBox.DefaultText = "";
+            this.descriptionTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.descriptionTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.descriptionTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.descriptionTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.descriptionTextBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(27)))), ((int)(((byte)(30)))));
+            this.descriptionTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.descriptionTextBox.ForeColor = System.Drawing.Color.White;
+            this.descriptionTextBox.Location = new System.Drawing.Point(35, 147);
+            this.descriptionTextBox.MaxLength = 600;
+            this.descriptionTextBox.MinimumSize = new System.Drawing.Size(0, 66);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.PlaceholderForeColor = System.Drawing.Color.Silver;
+            this.descriptionTextBox.PlaceholderText = "";
+            this.descriptionTextBox.SelectedText = "";
+            this.descriptionTextBox.Size = new System.Drawing.Size(176, 66);
+            this.descriptionTextBox.TabIndex = 51;
+            // 
+            // WriteNotifyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(20)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(246, 286);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WriteNotifyForm";
             this.Text = "WritwNotifyForm";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm FormRounded;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2CircleButton closeButton;
+        private Guna.UI2.WinForms.Guna2Button sendNotifyButton;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox titleTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private Guna.UI2.WinForms.Guna2TextBox descriptionTextBox;
     }
 }
--- Biblio/AppForms/AdminForms/WriteNotifyForm.cs
+++ Biblio/AppForms/AdminForms/WriteNotifyForm.cs
@@ -16,5 +16,10 @@ namespace Biblio.AppForms.AdminForms
         {
             InitializeComponent();
         }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
