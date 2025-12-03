128b7c1a00d23e4ac7fcd25c8716998805302a20
Лямкин Денис Сергеевич <lamkindenis127@gmail.com>
2025-12-03 17:01:16 +0300

    Cleaned up unused libraries.

--- Biblio/Models/UserReportCategory.cs
+++ Biblio/Models/UserReportCategory.cs
@@ -1,10 +1,8 @@
 namespace Biblio.Models
 {
-    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("UserReportCategory")]
     public partial class UserReportCategory
